ca46387215829c88ca2c9a84f1113417e30b4d5a
shdonas <shpdonas@gmail.com>
2020-09-04 11:11:20 -0400

    comment added

--- src/wolframalpha_api.py
+++ src/wolframalpha_api.py
@@ -1,14 +1,21 @@
 import wolframalpha
 from pip._vendor.distlib.compat import raw_input
 
+# appID/apiKey from WolframAlpha
+api_id = "JH5JK7-E87Y6G3W8K"
 
-def api_response(api_Id, question):
-    api_id = "JH5JK7-E87Y6G3W8K"
+
+# this app will return response from the WolframAplha
+def api_response(question):
+    # client request to connect to WolframAplha using the api key
     client = wolframalpha.Client(api_id)
+
+    # response from the WRA based on the client question
     response = client.query(question)
     return response
 
 
+# this app will fetch answer using the response from WRA server
 def fetch_ans(api_response):
     answer = next(api_response.results).text
     print(answer)
@@ -16,8 +23,7 @@ def fetch_ans(api_response):
 
 
 if __name__ == '__main__':
-    api_id = "JH5JK7-E87Y6G3W8K"
-    print("Hello World")
+    print("Hello Friends!!")
 
     question = raw_input("Question: ")
-    fetch_ans(api_response(api_id, question))
+    fetch_ans(api_response(question))
